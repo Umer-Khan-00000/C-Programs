49f26af7c1fe7471cf48f164181895d7b7280bc6
Umer Khan <91822670+Umer-Khan-00000@users.noreply.github.com>
2024-09-21 23:50:20 +0500

    Escape Sequensea

---
+++ Escape Sequences/Code.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "\n"; // Creates a new line
+    cout << "\t"; // Causes a space
+    cout << "Classs\b-X"; // Causes a backspace 
+    cout << "\a"; // Causes a beep in the computer
+    
+
+    return 0;
+}
+
+
+
+
